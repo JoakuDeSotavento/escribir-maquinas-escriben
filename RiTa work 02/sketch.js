706e0d7e6050b4f1a3bc973d127514653718b828
Joaku De Sotavento <joaquindiazescultor@gmail.com>
2020-11-04 11:41:19 +0100

    ejemplo rita 2

---
+++ RiTa work 02/sketch.js
@@ -0,0 +1,36 @@
+let input;
+let button;
+let lexicon;
+
+function setup(){
+    noCanvas();
+    lexicon = new RiLexicon();
+    input = createInput("Espect poison from standing water.");
+    button = createButton("Envia");
+    input.changed(processRita);
+    button.mousePressed(processRita);
+    input.size(300);
+}
+
+function processRita(){
+    let s = input.value();
+    let rs = new RiString(s);
+    let rWords = rs.words();
+    let rPos = rs.pos();
+    
+    console.log(rWords);
+    console.log(rPos);
+    
+    let output = " ";
+    for(let i = 0; i < rWords.length; i++){
+        
+        if(rPos[i] === "nn"){
+            output += lexicon.randomWord("nn", 6);
+        }else{
+            output += rWords[i];   
+        }
+            output += " ";
+    }
+    createP(output);
+    
+}
