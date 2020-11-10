65f64976ece57f66cab12a53d3d125b1425cee0e
Joaku De Sotavento <joaquindiazescultor@gmail.com>
2020-11-10 23:20:15 +0100

    Ejemplos de cámaras y video y toeria del color

---
+++ video-example-01/sketch.js
@@ -0,0 +1,26 @@
+let fingers;
+
+function setup() {
+  createCanvas(710, 400);
+  // especificar múltiples formatos para diferentes navegadores
+  fingers = createVideo('sonic.mp4');
+  fingers.hide(); // por defecto el video aparece en un elemento dom separado.
+  // escóndelo y dibújalo en el lienzo en vez de eso.
+}
+
+function draw() {
+  background(150);
+  image(fingers, 10, 10); // dibuja el cuadro del video en el lienzo.
+  filter(GRAY);
+  image(fingers, 150, 150); // dibuja una segunda copia en el lienzo.
+}
+
+function mousePressed() {
+  fingers.play(); // configurar el video para empezar a reproducirse en bucle
+    
+}
+
+function mouseReleased() {
+  fingers.stop(); // configurar el video para empezar a reproducirse en bucle
+    
+}
---
+++ video-example-02/sketch.js
@@ -0,0 +1,31 @@
+let fingers;
+
+function setup() {
+    createCanvas(1080, 720);
+  // especifica múltples formatos para distintos navegadores
+    fingers = createVideo("sonic.mp4");
+    //fingers.loop();
+    //fingers.hide();
+ 
+  noStroke();
+  fill(0);
+}
+
+function draw() {
+  background(255);
+  fingers.loadPixels();
+  const stepSize = round(constrain(mouseX / 8, 6, 32));
+  for (let y = 0; y < height; y += stepSize) {
+    for (let x = 0; x < width; x += stepSize) {
+      const i = y * width + x;
+      const darkness = (255 - fingers.pixels[i * 4]) / 255;
+      const radius = stepSize * darkness;
+      ellipse(x, y, radius, radius);
+    }
+  }
+}
+
+function mousePressed(){
+    fingers.loop();
+    fingers.hide();
+}
