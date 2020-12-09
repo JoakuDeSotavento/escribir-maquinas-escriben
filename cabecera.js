a23ea5fc036b2d5ffa5777bec53d4c78fa6c08fe
Joaku De Sotavento <joaquindiazescultor@gmail.com>
2020-12-09 21:06:39 +0100

    penultima clase del taller

---
+++ cabecera.js
@@ -0,0 +1,23 @@
+/*
+                                                                                                                                   
+///////////////////////////////////////////////////////
+Programa: 
+Autores: 
+Programador: 
+Musica: 
+Idea: Sylvia Molina, Javier Osona, Julio Sanz-Vázquez
+
+
+//////////////////////////////////////////////////////
+Instrucciones:
+
+
+//////////////////////////////////////////////////////
+Contacto
+
+e-mail:
+https://github.com/kronihias/head-pose-estimation
+
+///////////////////////////////////////////////////                                                 
+
+*/
